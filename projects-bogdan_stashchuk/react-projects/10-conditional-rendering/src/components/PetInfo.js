8edd2f226a7bfc5fb3072e6f3c35309c015b020b
sergeybabann <sergeybabann@yandex.ru>
2024-04-11 17:36:57 +0300

    additions

---
+++ projects-bogdan_stashchuk/react-projects/10-conditional-rendering/src/components/PetInfo.js
@@ -0,0 +1,11 @@
+function PetInfo(props) {
+  const { animal, age, hasPet } = props
+
+  return hasPet ? (
+    <h1>{`My ${animal} is ${age} years old`}</h1>
+  ) : (
+    <h2>I don't have an animal</h2>
+  )
+}
+
+export default PetInfo
---
+++ projects-bogdan_stashchuk/react-projects/20-useEffect/src/App.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [todo, setTodo] = useState(null)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/5')
+      .then((response) => response.json())
+      .then((json) => setTodo(json))
+  }, [])
+
+  console.log('App rendered')
+  console.log(todo)
+
+  return <div className="App">{todo && <h1>{todo.title}</h1>}</div>
+}
+
+export default App
